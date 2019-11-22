10783be60cac1618ae08127d978e73f5d821e75c
jhfelizardo12 <58068601+jhfelizardo12@users.noreply.github.com>
2019-11-22 17:42:32 +0800

    Update App.js
    test1111

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaaaaaaa
+          Learn aaaaaaaaa
         </a>
       </header>
     </div>
