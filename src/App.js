56e5180857c6e38669085b2d170b5f60f3c74a31
jhfelizardo12 <58068601+jhfelizardo12@users.noreply.github.com>
2019-11-22 17:34:50 +0800

    Update App.js
    test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaa
+          Learn Reactaaaaaaa
         </a>
       </header>
     </div>
